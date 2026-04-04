0a012e768b2f1ce9a3210d1d14cbdf61b4a268aa
Siwar-Raouafi <siwar.raouafi1@gmail.com>
2026-04-04 13:31:46 +0100

    Mise à jour

--- src/Entity/Bilan.php
+++ src/Entity/Bilan.php
@@ -11,6 +11,7 @@ class Bilan
 {
 
     #[ORM\Id]
+    #[ORM\GeneratedValue]
     #[ORM\Column(type: "integer")]
     private int $id;
 
--- src/Entity/Budget_previsionnel.php
+++ src/Entity/Budget_previsionnel.php
@@ -11,6 +11,7 @@ class Budget_previsionnel
 {
 
     #[ORM\Id]
+    #[ORM\GeneratedValue]
     #[ORM\Column(type: "integer")]
     private int $id;
 
